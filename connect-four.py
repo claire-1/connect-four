6f085879e61d69cc8299fd4849a903074ff87e8d
claire-1 <claire8991@gmail.com>
2020-03-14 16:34:43 -0400

    Simple connect-four board

--- connect-four.py
+++ connect-four.py
@@ -1,85 +1,29 @@
-{
- "cells": [
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "# TODOs\n",
-    "need to fill out other notebook file for submission\n",
-    "need to include documation + screenshots\n",
-    "need to evaluate if liked/didn't like python \n",
-    "SEE PROJECT DESCRIPTION PAGE FOR MORE INFO\n",
-    "\n",
-    "\n"
-   ]
-  },
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "# Project Idea\n",
-    "battleship game\n",
-    "\n",
-    "--> need a GUI; make it simple to begin with \n",
-    "--> need to make a grid --> probably best to use a dictionary for each square in the boards for the two players\n",
-    "--> "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+import pygame
+
+BLUE = (0, 128, 255)
+ORANGE = (255, 100, 0)
+
+
+pygame.init()
+screen = pygame.display.set_mode((900, 800))
+done = False
+clear_board = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            clear_board = True  # clear the board when you press the space key
+    if clear_board:
+        # TODO
+        color = BLUE
+    else:
+        # TODO
+        color = ORANGE
+
+    pygame.draw.circle(screen, color, (300, 60), 40)
+    pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+
+    pygame.display.flip()  # need to make any updates to the screen to be visible
