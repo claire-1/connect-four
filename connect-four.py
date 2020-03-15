d74a0553c0b948a0488d333fd9ddc2cd4bc16bce
claire-1 <claire8991@gmail.com>
2020-03-15 11:37:20 -0400

    WIP need to find the column where the click is

--- connect-four.py
+++ connect-four.py
@@ -20,22 +20,30 @@ PLAYER_2 = 2
 
 def set_up_board(screen):
     board_spaces_locations = dict()
+    board_column_locations = dict()
     board = dict()
     color = BLUE
     screen.fill(pygame.color.THECOLORS['mediumblue'])
     for i in range(1, VERTICAL_SPACES):
+        board_column_heap = list()
         for j in range(1, HORIZONTAL_SPACES):
+
             screen_location = (math.floor(j*(SCREEN_HEIGHT /
                                              VERTICAL_SPACES) - RADIUS), math.floor(i*(SCREEN_WIDTH/HORIZONTAL_SPACES) - RADIUS))
             pygame.draw.circle(screen, OPEN_TILE_COLOR,
                                screen_location, RADIUS)
             board_spaces_locations[screen_location] = (i, j)
-            board[(i, j)] = EMPTY
+
+            board_column_heap.append(j, screen_location)
+
+            board[(i, j)] = EMPTY  # TODO maybe don't need
+        # TODO how going to get all spaces in column?
+        board_column_locations[i] = board_column_heap
   #  pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
 
     pygame.display.update()  # need to make any updates to the screen to be visible
 
-    return (board_spaces_locations, board)
+    return (board_spaces_locations, board, board_column_locations)
 
 
 def clear_board():
@@ -53,7 +61,7 @@ def game_status(screen, board_spaces):
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    (board_spaces_locations, board) = set_up_board(screen)
+    (board_spaces_locations, board, board_column_locations) = set_up_board(screen)
     playing = True
     clear_board = False
     whose_move = PLAYER_1
@@ -70,11 +78,15 @@ def main():
             # TODO here
 
             (clicked_x, clicked_y) = event.pos
-            for screen_location in board_spaces_locations:
+            for (screen_location, board_location) in board_spaces_locations:
                 x_squared = (clicked_x - screen_location[0])**2
                 y_squared = (clicked_y - screen_location[1])**2
 
                 if (math.sqrt(x_squared + y_squared)) < 60:
+                    (row, col) = board_location
+                    # TODO here -- trying to get the lowest filled in space
+                    heapq.pop(board_column_locations[VERTICAL_SPACES - col])
+
                     if whose_move == PLAYER_1:
                         pygame.draw.circle(screen, PLAYER_1_TITLE_COLOR,
                                            screen_location, RADIUS)
@@ -83,8 +95,8 @@ def main():
                         pygame.draw.circle(screen, PLAYER_2_TITLE_COLOR,
                                            screen_location, RADIUS)
                         whose_move = PLAYER_1
-                    print("IN SQUARE " +
-                          str(board_spaces_locations[screen_location]))
+                    # TODO need to find the last filled in location in a column and then fill in that spot
+                    # really need a dictionary that allows for searching on both the key and the value
 
         if clear_board:
             # TODO
